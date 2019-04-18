4cceb642140e77dc73671d32de35aa92a70567dd
Max Ehrlich <max.ehr@gmail.com>
2019-04-18 13:14:00 -0400

    Add compression test

---
+++ test_compress.py
@@ -0,0 +1,21 @@
+import torch
+import torchjpeg
+import argparse
+from PIL import Image
+from torchvision.transforms.functional import to_tensor
+import cv2
+import numpy as np
+
+
+parser = argparse.ArgumentParser(
+    'Tests the pytorch DCT loader by reading and image, quantizing its pixels, and writing the DCT coefficients to a JPEG'
+)
+parser.add_argument('input', help='Input image, should be lossless')
+parser.add_argument('output', help='Output image, must be a JPEG')
+parser.add_argument('quality', type=int, help='Output quality on the 0-100 scale')
+args = parser.parse_args()
+
+im = to_tensor(Image.open(args.input))
+
+dimensions, quantization, Y_coefficients, CbCr_coefficients = torchjpeg.quantize_at_quality(im, args.quality)
+torchjpeg.write_coefficients(args.output, dimensions, quantization, Y_coefficients, CbCr_coefficients)
