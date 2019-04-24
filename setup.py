f829b8d2024260d9f2ed6d14c4f1bbd739d55777
Max Ehrlich <max.ehr@gmail.com>
2019-04-24 09:48:09 -0400

    Use any libjpeg and hope it works

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from torch.utils.cpp_extension import CppExtension, BuildExtension
 
 setup(name='torchjpeg',
-      ext_modules=[CppExtension('torchjpeg', ['torchjpeg.cpp', 'jdatadst.c'], libraries=[':libjpeg.so.8'])],
+      ext_modules=[CppExtension('torchjpeg', ['torchjpeg.cpp', 'jdatadst.c'], libraries=['jpeg'])],
       cmdclass={'build_ext': BuildExtension})
