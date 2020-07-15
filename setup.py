e823a31c2261dffbc072603df8435964213c2bba
Max Ehrlich <max.ehr@gmail.com>
2020-07-15 10:25:20 -0400

    Move codec functions to submodule

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 from torch.utils.cpp_extension import CppExtension, BuildExtension
 
-setup(name='torchjpeg',
-      ext_modules=[CppExtension('torchjpeg', ['torchjpeg.cpp', 'jdatadst.c'], libraries=['jpeg'])],
+setup(name='torchjpeg.codec',
+      ext_modules=[CppExtension('torchjpeg', ['codec/torchjpeg.cpp', 'codec/jdatadst.c'], libraries=['jpeg'])],
       cmdclass={'build_ext': BuildExtension},
       install_requires=[
           'torch',
