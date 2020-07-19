a3dc793c97d0f07c147941933225a1a57a2e5fb6
Max Ehrlich <max.ehr@gmail.com>
2020-07-19 09:39:34 -0400

    Refactor module name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from torch.utils.cpp_extension import CppExtension, BuildExtension
 
 setup(name='torchjpeg',
       packages=find_packages(),
-      ext_modules=[CppExtension('torchjpeg.codec.codec_ops', ['torchjpeg/codec/codec_ops.cpp', 'torchjpeg/codec/jdatadst.cpp'], libraries=['jpeg'], extra_compile_args=['-std=c++17'])],
+      ext_modules=[CppExtension('torchjpeg.codec._codec_ops', ['torchjpeg/codec/codec_ops.cpp', 'torchjpeg/codec/jdatadst.cpp'], libraries=['jpeg'], extra_compile_args=['-std=c++17'])],
       cmdclass={'build_ext': BuildExtension},
       install_requires=[
           'torch',
