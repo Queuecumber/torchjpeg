d4167f3ff9ce1edc9bd8f4e28ff3f408b3fad914
Max Ehrlich <max.ehr@gmail.com>
2020-07-31 09:59:02 -0400

    Include libjpeg Source
    Depending on downloading the external libjpeg source causes problems, so pull it into the project. This will simplify the build, make it easily reproducible, and make it possible to be greatly sped up for CI
    
    MR !7 squashed commit
    
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- build.py
+++ build.py
@@ -1,8 +1,12 @@
 # pylint: disable=missing-function-docstring
+from subprocess import check_call
+
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 
 def build(setup_kwargs):
+    check_call("cd src/libjpeg && ./configure --enable-static --with-pic && make clean && make", shell=True)
+
     setup_kwargs.update(
-        {"ext_modules": [CppExtension("torchjpeg.codec._codec_ops", ["src/torchjpeg/codec/codec_ops.cpp",], include_dirs=["/usr/local/include"], extra_objects=["/usr/local/lib/libjpeg.a"], extra_compile_args=["-std=c++17"],)], "cmdclass": {"build_ext": BuildExtension},}
+        {"ext_modules": [CppExtension("torchjpeg.codec._codec_ops", ["src/torchjpeg/codec/codec_ops.cpp",], include_dirs=["src/libjpeg"], extra_objects=["src/libjpeg/.libs/libjpeg.a"], extra_compile_args=["-std=c++17"],)], "cmdclass": {"build_ext": BuildExtension},}
     )
