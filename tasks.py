1993b0d71caf7a99132796d91c89fa9614610f92
Max Ehrlich <max.ehr@gmail.com>
2020-07-31 14:18:31 -0400

    Add Helper Tasks
    This MR makes it easier to test and squash. Since we have a specific workflow that we're enforcing, these tasks make it easier on people.
    
    Run
    
    ```
    poetry run invoke test
    ```
    
    which takes optional arguments "all" (default), "import", "unit", "e2e" to run the tests without manually invoking pytest with the folders
    
    Run
    
    ```
    poetry run invoke squashhelp
    ```
    
    to print out the rebase command to use for squashing your commits. Optional argument --short prints only the rebase command, default includes some more information.
    
    MR !8 squashed commit
    
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- tasks.py
+++ tasks.py
@@ -53,10 +53,10 @@ def style(c):
 def dco(c):
     print(f"{Fore.BLUE}Checking For DCO{Style.RESET_ALL}")
 
-    c.run("git remote add upstream-dco-check https://gitlab.com/Queuecumber/torchjpeg.git", warn=True)
-    c.run("git fetch -n upstream-dco-check", warn=True)
-    r = c.run('git rev-list HEAD ^remotes/upstream-dco-check/master --count --grep "Signed-off-by: .*( <.*@.*>)?$" --invert-grep --extended-regexp', warn=True)
-    c.run("git remote remove upstream-dco-check", warn=True)
+    c.run("git remote add upstream-dco-check https://gitlab.com/Queuecumber/torchjpeg.git", warn=True, hide=True)
+    c.run("git fetch -n upstream-dco-check", warn=True, hide=True)
+    r = c.run('git rev-list HEAD ^remotes/upstream-dco-check/master --count --grep "Signed-off-by: .*( <.*@.*>)?$" --invert-grep --extended-regexp', warn=True, hide=True)
+    c.run("git remote remove upstream-dco-check", warn=True, hide=True)
 
     cnt = int(r.stdout)
 
@@ -78,3 +78,34 @@ def cq(c):
         style(c)
         type_checking(c)
         lint(c)
+
+
+@task
+def test(c, which="all"):
+    test_on: List[str] = []
+    if which == "all":
+        test_on += ["import", "unit", "e2e"]
+    else:
+        test_on.append(which)
+
+    print(f"{Fore.BLUE}Testing: {Fore.GREEN}{test_on}{Style.RESET_ALL}")
+
+    for t in test_on:
+        r = c.run(f"pytest test/{t}", warn=True)
+        if r.exited != 0:
+            print(f"{Fore.GREEN}{t}{Fore.RED} tests FAILED{Style.RESET_ALL}")
+
+
+@task
+def squashhelp(c, short=False):
+    c.run("git remote add upstream-squashhelp https://gitlab.com/Queuecumber/torchjpeg.git", warn=True, hide=True)
+    c.run("git fetch -n upstream-squashhelp", hide=True)
+    rev = c.run("git merge-base HEAD upstream-squashhelp/master", warn=True, hide=True)
+    c.run("git remote remove upstream-squashhelp", warn=True, hide=True)
+
+    if not short:
+        print(f"{Fore.BLUE}The {Fore.YELLOW}merge base{Fore.BLUE} between your branch and {Fore.GREEN}upstream{Fore.BLUE} is commit {Fore.YELLOW}{rev.stdout.strip()}{Fore.BLUE}.{Style.RESET_ALL}")
+        print(f"{Fore.BLUE}Based on this, the squash command should be {Fore.YELLOW}git rebase -i {rev.stdout.strip()[:8]}{Style.RESET_ALL}")
+        print(f"{Fore.BLUE}Remember to {Fore.RED}DCO sign-off{Fore.BLUE} your squashed commit and {Fore.GREEN}rebase on upstream{Fore.BLUE} when you're finished.{Style.RESET_ALL}")
+    else:
+        print(f"{Fore.YELLOW}git rebase -i {rev.stdout.strip()[:8]}{Style.RESET_ALL}")
