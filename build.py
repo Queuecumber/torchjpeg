2dfc1f3995cdfe841062989a2ac87e77553ef834
Max Ehrlich <max.ehr@gmail.com>
2020-07-31 17:16:51 -0400

    Faster CI
    This MR cuts the CI build time from ~15 min to ~5min using caching and DAG. It also adds the test result report so it can be displayed with the MR.
    
    MR !9 squashed commit
    
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- build.py
+++ build.py
@@ -4,9 +4,17 @@ from subprocess import check_call
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 
+def build_libjpeg():
+    check_call("cd src/libjpeg && ./configure --enable-static --with-pic && make", shell=True)
+
+
 def build(setup_kwargs):
-    check_call("cd src/libjpeg && ./configure --enable-static --with-pic && make clean && make", shell=True)
+    build_libjpeg()
 
     setup_kwargs.update(
         {"ext_modules": [CppExtension("torchjpeg.codec._codec_ops", ["src/torchjpeg/codec/codec_ops.cpp",], include_dirs=["src/libjpeg"], extra_objects=["src/libjpeg/.libs/libjpeg.a"], extra_compile_args=["-std=c++17"],)], "cmdclass": {"build_ext": BuildExtension},}
     )
+
+
+if __name__ == "__main__":
+    build_libjpeg()
