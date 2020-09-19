96089cc841a9bc1be02cc57972dafae7fadcf9d5
Max Ehrlich <max.ehr@gmail.com>
2020-09-19 19:55:47 -0400

    Fix Zigzag Ordering
    Zigzag indices were not correct, some were repeated or missing
    
    MR !16 single commit
    
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- src/torchjpeg/dct/__init__.py
+++ src/torchjpeg/dct/__init__.py
@@ -374,9 +374,9 @@ def zigzag(coefficients: Tensor) -> Tensor:
          2,  4,  7, 13, 16, 26, 29, 42,
          3,  8, 12, 17, 25, 30, 41, 43,
          9, 11, 18, 24, 31, 40, 44, 53,
-        10, 19, 23, 32, 38, 45, 52, 54,
+        10, 19, 23, 32, 39, 45, 52, 54,
         20, 22, 33, 38, 46, 51, 55, 60,
-        21, 34, 36, 47, 50, 56, 59, 61,
+        21, 34, 37, 47, 50, 56, 59, 61,
         35, 36, 48, 49, 57, 58, 62, 63 
     ]).long() 
     # fmt: on
@@ -389,7 +389,7 @@ def zigzag(coefficients: Tensor) -> Tensor:
     c = blockify(c, 8)
 
     c = c.view(c.shape[0], c.shape[1], c.shape[2], 64)
-    c[:, :, :, :] = c[:, :, :, zigzag_indices]
+    c = c[..., zigzag_indices]
 
     if len(coefficients.shape) == 3:
         c = c.squeeze(0)
